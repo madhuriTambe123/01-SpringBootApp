d1b0a9ad3d048d3013c90cbb4f730b15fadf0994
madhuriTambe123 <madhuri3412@gmail.com>
2020-12-05 15:11:25 +0530

    initial commit

---
+++ src/main/java/com/tl/Test01SpringBootGitApplication.java
@@ -0,0 +1,13 @@
+package com.tl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test01SpringBootGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test01SpringBootGitApplication.class, args);
+	}
+
+}
