2ac29dae78e00413f73d30587b53ea5ddbb4b4b7
madhuriTambe123 <madhuri3412@gmail.com>
2020-12-05 15:36:25 +0530

    first commit for boot

--- src/main/java/com/tl/Test01SpringBootGitApplication.java
+++ src/main/java/com/tl/Test01SpringBootGitApplication.java
@@ -2,12 +2,17 @@ package com.tl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import ch.qos.logback.classic.joran.action.ConfigurationAction;
 
 @SpringBootApplication
 public class Test01SpringBootGitApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Test01SpringBootGitApplication.class, args);
+		ConfigurableApplicationContext ctx = SpringApplication.run(Test01SpringBootGitApplication.class, args);
+		ctx.close();
+		// Commeted code
 	}
 
 }
